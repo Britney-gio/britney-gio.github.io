0742ed4246bcd537aa32e233cecb29b241ca036e
Utente <giorgianieli@gmail.com>
2024-10-15 22:18:17 +0200

    Clear code and add js form contatto

---
+++ script.js
@@ -0,0 +1,13 @@
+function sendMail (){
+    let parms = {
+        nome : document.getElementById("nome").value,
+        cognome : document.getElementById("email").value,
+        mail : document.getElementById("mail").value,
+        oggetto : document.getElementById("ogetto").value,
+        messaggio : document.getElementById("messaggio").value,
+    }
+
+    emailjs.send("service_xfkznvd","template_6focxw9",parms).then(alert("Mail Inviata!!"))
+
+
+}
